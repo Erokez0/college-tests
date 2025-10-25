d151212dc4aba56e7dc53f706b2e96805a93c171
erokez <parshin.k.07@gmail.com>
2025-10-25 15:26:01 +0300

    added a redundant space for linter to complain about

--- src/checkers/has-lower-case-letters.ts
+++ src/checkers/has-lower-case-letters.ts
@@ -2,5 +2,5 @@ import { ValidationResult } from "../interfaces";
 import { hasRegex } from "./has-regex";
 
 export function hasLowerCaseLetters(str: string, neededAmount: number): ValidationResult {
-    return hasRegex(str, /\p{Ll}/gu, "строчных букв", neededAmount);
+    return  hasRegex(str, /\p{Ll}/gu, "строчных букв", neededAmount);
 }
